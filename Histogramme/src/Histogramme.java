d5efe47277df93bbe727f06029dfa01ed320610a
Paul Mabileau <paulmabileau@hotmail.fr>
2020-11-17 17:00:33 +0100

    Reformat and refactor Histogramme
    Signed-off-by: Paul Mabileau <paulmabileau@hotmail.fr>

--- Histogramme/src/Histogramme.java
+++ Histogramme/src/Histogramme.java
@@ -1,38 +1,32 @@
-
 public class Histogramme {
+    public static void main(final String[] args) {
+        final int[] tab = new int[args.length];
+        int max = 0, min = 0;
 
-	public static void main(String[] args) {
-		
-		int[] tab = new int[args.length];
-		int max = 0, min = 0;
-		
-		for (int i = 0; i < tab.length; i++) {
-			tab[i] = Integer.parseInt(args[i]);
-			
-			if (tab[i] > max) {
-				max = tab[i];
-			}
-			
-			if (tab[i] < min) {
-				min = tab[i];
-			}
-		}
-		
-		for (int i = max; i >= min; i--) {
-			for (int j = 0; j < tab.length; j++) {
-				if ((i == 0 && tab[j] == 0) || (i > 0 && tab[j] > 0 && tab[j] >= i) || (i < 0 && tab[j] < 0 && tab[j] <= i)) {
-					System.out.print('*');
-				}
-				else if (i == 0 && tab[j] != 0) {
-					System.out.print('+');
-				}
-				else {
-					System.out.print(' ');
-				}
-			}
-			System.out.println();
-		}
-		
-	}
+        for (int i = 0; i < tab.length; i++) {
+            tab[i] = Integer.parseInt(args[i]);
 
+            if (tab[i] > max) {
+                max = tab[i];
+            }
+
+            if (tab[i] < min) {
+                min = tab[i];
+            }
+        }
+
+        for (int i = max; i >= min; i--) {
+            for (final int element: tab) {
+                if (i == 0 && element == 0 || i > 0 && element > 0 && element >= i
+                        || i < 0 && element < 0 && element <= i) {
+                    System.out.print('*');
+                } else if (i == 0 && element != 0) {
+                    System.out.print('+');
+                } else {
+                    System.out.print(' ');
+                }
+            }
+            System.out.println();
+        }
+    }
 }
