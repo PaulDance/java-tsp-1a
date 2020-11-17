03bd20fd152b71f3084841f33491e6d4880639c6
Paul Mabileau <paulmabileau@hotmail.fr>
2020-11-17 17:41:02 +0100

    Reformat and refactor PGCD
    Signed-off-by: Paul Mabileau <paulmabileau@hotmail.fr>

--- PGCD/src/PGCD.java
+++ PGCD/src/PGCD.java
@@ -1,28 +1,24 @@
-
 public class PGCD {
+    public static void main(final String[] args) {
+        if (args.length != 2) {
+            System.out.println("Il faut deux arguments.");
+            System.exit(1);
+        }
 
-	public static void main(String[] args) {
-		
-		if (args.length != 2) {
-			System.out.println("Il faut deux arguments.");
-			System.exit(1);
-		}
-		
-		int a = Integer.parseInt(args[0]), b = Integer.parseInt(args[1]), tmp = 0;
-		
-		if (a < b) {
-			tmp = a;
-			a = b;
-			b = tmp;
-		}
-		
-		while (b != 0) {
-			tmp = b;
-			b = a - b * (a / b);
-			a = tmp;
-		}
-		
-		System.out.println(a);
-	}
+        int a = Integer.parseInt(args[0]), b = Integer.parseInt(args[1]), tmp = 0;
 
+        if (a < b) {
+            tmp = a;
+            a = b;
+            b = tmp;
+        }
+
+        while (b != 0) {
+            tmp = b;
+            b = a - b * (a / b);
+            a = tmp;
+        }
+
+        System.out.println(a);
+    }
 }
