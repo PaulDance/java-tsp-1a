bbcbc6dbb9db3e028867480dfc6d14cfdbb001b0
Paul Mabileau <paulmabileau@hotmail.fr>
2020-11-17 16:55:02 +0100

    Reformat and refactor Calculette
    Signed-off-by: Paul Mabileau <paulmabileau@hotmail.fr>

--- Calculette/src/Calculette.java
+++ Calculette/src/Calculette.java
@@ -1,19 +1,14 @@
-
 public class Calculette {
+    public static void main(final String[] args) {
+        final int[] tab = new int[args.length];
+        int somme = 0;
 
-	public static void main(String[] args) {
-		
-		int[] tab = new int[args.length];
-		int somme = 0;
-		
-		for (int i = 0; i < tab.length; i++) {
-			tab[i] = Integer.parseInt(args[i]);
-			System.out.println("args[" + i +"] : " + tab[i]);
-			somme += tab[i];
-		}
-		
-		System.out.println("Somme : " + somme);
+        for (int i = 0; i < tab.length; i++) {
+            tab[i] = Integer.parseInt(args[i]);
+            System.out.println("args[" + i + "] : " + tab[i]);
+            somme += tab[i];
+        }
 
-	}
-
+        System.out.println("Somme : " + somme);
+    }
 }
