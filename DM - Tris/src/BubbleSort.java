815e3e8c21f6edee1596e092ccb3996b48f5c9ca
Paul Mabileau <paulmabileau@hotmail.fr>
2020-11-17 16:55:18 +0100

    Reformat and refactor DM Tris
    Signed-off-by: Paul Mabileau <paulmabileau@hotmail.fr>

--- DM - Tris/src/BubbleSort.java
+++ DM - Tris/src/BubbleSort.java
@@ -1,39 +1,45 @@
 /*
  * Si une variable doit être désignée dans un commentaire, elle sera entre <>.
- * Les tableaux utilisés ici sont générés aléatoirement.
- * Si vous le souhaitez, l'exemple du sujet est disponible en commentaire.
-*/
-
+ * Les tableaux utilisés ici sont générés aléatoirement. Si vous le souhaitez,
+ * l'exemple du sujet est disponible en commentaire.
+ */
 
 public class BubbleSort {
+    public static void main(final String[] args) {
+        // Amplitude de la génération et variable temporaire pour réaliser les
+        // échanges.
+        final double ampli = 10;
+        double tmp = 0;
+        // Tableau de doubles nuls.
+        final double[] tab = new double[5];
+        // double[] tab = {2.3, 17.0, 3.14, 8.83, 7.26};
 
-	public static void main(String[] args) {
-		double ampli = 10, tmp = 0;								// Amplitude de la génération et variable temporaire pour réaliser les échanges.
-		double[] tab = new double[5];							// Tableau de doubles nuls.
-		//double[] tab = {2.3, 17.0, 3.14, 8.83, 7.26};			// Exemple du sujet.
-		
-		System.out.println("Tableau initial :");
-		for (int i = 0; i < tab.length; i++) {
-			tab[i] = 2 * ampli * (Math.random() - 0.5);			// Initialisation du tableau dont les éléments sont aléatoires entre -<ampli> et <ampli>. A commenter pour avoir l'exemple du sujet.
-			System.out.print(tab[i] + " ");						// Affichage des éléments initiaux.
-		}
-		System.out.println("\n\nTableau trié :");
-		
-		
-		// Tri à bulles.
-		for (int i = tab.length - 1; i >= 1; i--) {				// On parcourt les éléments du tableau dans le sens inverse.
-			for (int j = 0; j <= i - 1; j++) {					// Chaque élément d'indice inférieur ou égal à <i - 1>
-				if (tab[j] > tab[j + 1]) {						// est comparé à son suivant
-					tmp = tab[j];								// pour être échangés s'il est plus grand.
-					tab[j] = tab[j + 1];
-					tab[j + 1] = tmp;
-				}
-			}
-		}
-		
-		
-		for (int i = 0; i < tab.length; i++) {
-			System.out.print(tab[i] + " ");						// Affichage des éléments du tableau après le tri.
-		}
-	}
+        // Initialisation du tableau dont les éléments sont aléatoires entre
+        // -<ampli> et <ampli>. A commenter pour avoir l'exemple du sujet.
+        System.out.println("Tableau initial :");
+        for (int i = 0; i < tab.length; i++) {
+            tab[i] = 2 * ampli * (Math.random() - 0.5);
+            System.out.print(tab[i] + " ");
+        }
+        System.out.println("\n\nTableau trié :");
+
+        // Tri à bulles.
+        // On parcourt les éléments du tableau dans le sens inverse.
+        for (int i = tab.length - 1; i >= 1; i--) {
+            // Chaque élément d'indice inférieur ou égal à <i - 1>
+            for (int j = 0; j <= i - 1; j++) {
+                // est comparé à son suivant pour être échangés si plus grand.
+                if (tab[j] > tab[j + 1]) {
+                    tmp = tab[j];
+                    tab[j] = tab[j + 1];
+                    tab[j + 1] = tmp;
+                }
+            }
+        }
+
+        // Affichage des éléments du tableau après le tri.
+        for (final double element: tab) {
+            System.out.print(element + " ");
+        }
+    }
 }
--- DM - Tris/src/InsertionSort.java
+++ DM - Tris/src/InsertionSort.java
@@ -1,44 +1,56 @@
 /*
  * Si une variable doit être désignée dans un commentaire, elle sera entre <>.
- * Les tableaux utilisés ici sont générés aléatoirement.
- * Si vous le souhaitez, l'exemple du sujet est disponible en commentaire.
-*/
-
+ * Les tableaux utilisés ici sont générés aléatoirement. Si vous le souhaitez,
+ * l'exemple du sujet est disponible en commentaire.
+ */
 
 public class InsertionSort {
+    public static void main(final String[] args) {
+        // Longueur du tableau et indice de tri.
+        final int len = 5;
+        int dest = 0;
+        // Amplitude de la génération.
+        final double ampli = 10;
+        // Tableau d'entrée, non initialisé pour l'instant.
+        final double[] tab = new double[len];
+        // Tableaux de sortie, gardé à zéro pour l'instant.
+        final double[] sorted = new double[len];
+        // double[] tab = {8, 6, 1, 3, 5, 2}; len = tab.length;
 
-	public static void main(String[] args) {
-		int len = 5, dest = 0;											// Longueur du tableau et indice de tri.
-		double ampli = 10;												// Amplitude de la génération.
-		double[] tab = new double[len];									// Tableau d'entrée, non initialisé pour l'instant.
-		double[] sorted = new double[len];								// Tableaux de sortie, gardé à zéro pour l'instant.
-		//double[] tab = {8, 6, 1, 3, 5, 2}; len = tab.length;			// Exemple du sujet.
-		
-		System.out.println("Tableau initial :");
-		for (int i = 0; i < tab.length; i++) {
-			tab[i] = 2 * ampli * (Math.random() - 0.5);					// Initialisation du tableau dont les éléments sont aléatoires entre <-ampli> et <ampli>.
-			System.out.print(tab[i] + " ");								// Affichage des éléments initiaux.
-		}
-		System.out.println("\n\nTableau trié :");
-		
-		//Tri par insertion :
-		for (int i = 0; i < len; i++) {									// On traverse tous les éléments du tableau initial.
-			dest = 0;													// L'indice de destination est remis à zéro à chaque nouvel élément.
-			
-			while (dest < i && tab[i] > sorted[dest]) {					// On cherche l'indice de destination jusqu'au nombre d'éléments insérés jusqu'ici, c'est-à-dire <i>.
-				dest += 1;
-			}
-			
-			for (int j = len - 1; j > dest; j--) {						// Une fois l'indice de destination trouvé, on décale tous les éléments à sa droite,
-				sorted[j] = sorted[j - 1];
-			}
-			
-			sorted[dest] = tab[i];										// et on met l'élément d'indide i à la destination.
-		}
-		
-		
-		for (int i = 0; i < len; i++) {
-			System.out.print(sorted[i] + " ");							// Affichage des éléments du tableau après le tri.
-		}
-	}
+        // Initialisation du tableau dont les éléments sont aléatoires entre
+        // <-ampli> et <ampli>.
+        System.out.println("Tableau initial :");
+        for (int i = 0; i < tab.length; i++) {
+            tab[i] = 2 * ampli * (Math.random() - 0.5);
+            System.out.print(tab[i] + " ");
+        }
+        System.out.println("\n\nTableau trié :");
+
+        // Tri par insertion :
+        // On traverse tous les éléments du tableau initial.
+        for (int i = 0; i < len; i++) {
+            // L'indice de destination est remis à zéro à chaque nouvel élément.
+            dest = 0;
+
+            // On cherche l'indice de destination jusqu'au nombre d'éléments
+            // insérés jusqu'ici, c'est-à-dire <i>.
+            while (dest < i && tab[i] > sorted[dest]) {
+                dest += 1;
+            }
+
+            // Une fois l'indice de destination trouvé, on décale tous les
+            // éléments à sa droite,
+            for (int j = len - 1; j > dest; j--) {
+                sorted[j] = sorted[j - 1];
+            }
+
+            // et on met l'élément d'indide i à la destination.
+            sorted[dest] = tab[i];
+        }
+
+        // Affichage des éléments du tableau après le tri.
+        for (int i = 0; i < len; i++) {
+            System.out.print(sorted[i] + " ");
+        }
+    }
 }
