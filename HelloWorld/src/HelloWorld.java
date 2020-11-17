66eaa72ed68cf984b25f0d516f704d89de483dd7
Paul Mabileau <paulmabileau@hotmail.fr>
2020-11-17 16:59:45 +0100

    Reformat and refactor HelloWorld
    Signed-off-by: Paul Mabileau <paulmabileau@hotmail.fr>

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,14 +1,9 @@
-
 public class HelloWorld {
+    public static void main(final String[] args) {
+        System.out.println("Hello, world!!!");
 
-	public static void main(String[] args) {
-		
-		System.out.println("Hello, world!!!");
-		
-		for (int i = 0; i < args.length; i++) {
-			System.out.println("args[" + i + "]: " + args[i]);
-		}
-		
-	}
-
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("args[" + i + "]: " + args[i]);
+        }
+    }
 }
