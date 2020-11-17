f5dba5937d091533e9e55bd1b1b514d3bae943bf
Paul Mabileau <paulmabileau@hotmail.fr>
2020-11-17 17:50:52 +0100

    Reformat and refactor Types
    Signed-off-by: Paul Mabileau <paulmabileau@hotmail.fr>

--- Types/src/Type.java
+++ Types/src/Type.java
@@ -1,37 +1,34 @@
-
 public class Type {
+    public static void main(final String[] args) {
+        final int x = 5;
+        final int y = x / 3;
+        final double d = 5;
+        final double e = d / 3;
+        final double f = x / 3;
+        final int g = (int) e; // type moins fort : conversion explicite
+                               // nécessaire
+        final char r = 'a';
+        final int s = r;
+        final int t = 98;
+        final char u = (char) t; // type moins fort : conversion explicite
+                                 // nécessaire
 
-	public static void main(String[] args) {
-		
-		int x = 5;
-		int y = x / 3;
-		double d = 5;
-		double e = d / 3;
-		double f = x / 3;
-		int g = (int)e;			// type moins fort : conversion explicite nécessaire
-		char r = 'a';
-		int s = r;
-		int t = 98;
-		char u = (char)t;		// type moins fort : conversion explicite nécessaire
-		
-		String s1 = "y vaut " + y;
-		String s2 = "e vaut " + e;
-		String s3 = "f vaut " + f;
-		String s4 = "g vaut " + g;
-		String s5 = "r vaut " + r;
-		String s6 = "s vaut " + s;
-		String s7 = "t vaut " + t;
-		String s8 = "u vaut " + u;
+        final String s1 = "y vaut " + y;
+        final String s2 = "e vaut " + e;
+        final String s3 = "f vaut " + f;
+        final String s4 = "g vaut " + g;
+        final String s5 = "r vaut " + r;
+        final String s6 = "s vaut " + s;
+        final String s7 = "t vaut " + t;
+        final String s8 = "u vaut " + u;
 
-		System.out.println(s1);
-		System.out.println(s2);
-		System.out.println(s3);
-		System.out.println(s4);
-		System.out.println(s5);
-		System.out.println(s6);
-		System.out.println(s7);
-		System.out.println(s8);
-
-	}
-
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+        System.out.println(s4);
+        System.out.println(s5);
+        System.out.println(s6);
+        System.out.println(s7);
+        System.out.println(s8);
+    }
 }
