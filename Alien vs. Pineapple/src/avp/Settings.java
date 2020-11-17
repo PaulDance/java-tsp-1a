373a1ced8b458ce0323a8058a9c4cda3ba53d293
Paul Mabileau <paulmabileau@hotmail.fr>
2020-11-17 16:54:08 +0100

    Reformat and refactor Alien vs. Pineapple
    Signed-off-by: Paul Mabileau <paulmabileau@hotmail.fr>

--- Alien vs. Pineapple/src/avp/Settings.java
+++ Alien vs. Pineapple/src/avp/Settings.java
@@ -1,11 +1,8 @@
 package avp;
 
 public final class Settings {
-	public static final double	ctrlAcc = 0.7;
-	public static final int		winWidth = 1041,
-								winHeight = 781,
-								xFix = 6,
-								yFix = 30,
-								maxAlienSpeed = 7,
-								numPines = 150;
+    public static final double ctrlAcc = 0.7;
+    public static final int winWidth = 1041, winHeight = 781,
+                            xFix = 6, yFix = 30,
+                            maxAlienSpeed = 7, numPines = 150;
 }
