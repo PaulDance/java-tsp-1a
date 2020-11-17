d400255c70736c32b26a9d87395a2c2d320ac318
Paul Mabileau <paulmabileau@hotmail.fr>
2020-11-17 17:49:25 +0100

    Reformat and refactor Shortest Path
    Signed-off-by: Paul Mabileau <paulmabileau@hotmail.fr>

--- Shortest Path/src/shortespath/Location.java
+++ Shortest Path/src/shortespath/Location.java
@@ -1,106 +1,170 @@
 package shortespath;
+
 import java.util.Iterator;
 import java.util.LinkedList;
 
 
-public class Location {									// Defines a vertex representing a city using a name and geodesic coordinates.
-	private String name;							// This structure can be used to implement graphs by calling setNeighbors.
-	private double latitude, longitude, distance;
-	private Location[] neighbors;					// The attribute <neighbors> is used to make the graph edges.
-	private Location from;							// <distance> and <from> are specifically meant for the path search.
-	
-	public Location(String name, double latitude, double longitude) {
-		this.name = name;
-		this.latitude = latitude;
-		this.longitude = longitude;
-		this.distance = Double.POSITIVE_INFINITY;
-		this.from = null;
-	}
-	
-	public Location(String name, double latitude, double longitude, Location... neighbors) {
-		this.name = name;
-		this.latitude = latitude;
-		this.longitude = longitude;
-		this.neighbors = neighbors;
-		this.distance = Double.POSITIVE_INFINITY;
-		this.from = null;
-	}
-	
-	public void display() {								// Standard display method.
-		System.out.println(this.name + ": (" + this.latitude + ", " + this.longitude + ")");
-	}
-	
-	private void distanceDisplay() {				// Display used only in the path search.
-		System.out.println("\t" + this.name + " at " + this.distance + " km");
-	}
-	
-	public double distanceTo(Location city) {				// Computes the length of the 3D arc going from Location <this> to <city> on the Earth' surface.
-		double lat1 = (Math.PI * this.latitude) / 180, lat2 = (Math.PI * city.latitude) / 180, long1 = (Math.PI * this.longitude) / 180, long2 = (Math.PI * city.longitude) / 180;
-		return 6378 * (Math.PI / 2 - Math.asin(Math.sin(lat2) * Math.sin(lat1) + Math.cos(long2 - long1) * Math.cos(lat2) * Math.cos(lat1)));
-	}
-	
-	public void setNeighbors(Location... neighbors) {		// Sets <this> current vertex' neighboring Locations to the given <neighbors> Location array.
-		this.neighbors = neighbors;
-	}
-	
-	public double getDistance() {							// Getter for path distance from starting point.
-		return this.distance;
-	}
-	
-	
-	public LinkedList<Location> pathTo(Location city) {	// Uses Dijkstra's graph path search algorithm to compute the shortest path from <this> to <city>.
-		LocationSet toVisit = new LocationSet(), newToVisit = new LocationSet();
-		toVisit.add(this);							// <toVisit> is a LocationSet containing the locations that have to be visited during the current step.
-		newToVisit.add(this);						// <newToVisit> is used to receive the next locations needing to be visited and give them to <toVisit> after a step.
-		this.distance = 0;							// Distance from start to start: 0.
-		
-		while (!toVisit.isEmpty()) {														// While there are still some locations to visit,
-			for (int i = 0; i < toVisit.numLocations; i++) {								// we visit them one by one,
-				if (toVisit.locations[i].neighbors != null) {								// and if the current location has neighbors,
-					for (int j = 0; j < toVisit.locations[i].neighbors.length; j++) {		// we go through them also;
-						if (toVisit.locations[i] != city									// if the accumulated distance to the current neighbor is smaller than previously,
-								&& toVisit.locations[i].distance + toVisit.locations[i].distanceTo(toVisit.locations[i].neighbors[j]) < toVisit.locations[i].neighbors[j].distance) {
-							toVisit.locations[i].neighbors[j].distance = toVisit.locations[i].distance + toVisit.locations[i].distanceTo(toVisit.locations[i].neighbors[j]);
-							toVisit.locations[i].neighbors[j].from = toVisit.locations[i];	// we update its parameters: the distance is corrected, the path now goes through it,
-							newToVisit.add(toVisit.locations[i].neighbors[j]);				// and it needs to be visited during next step.
-						}
-					}
-				}
-			}
-			toVisit.become(newToVisit);				// After each of every available location's neighbors has been processed,
-			newToVisit.clear();						// the next round of locations is loaded.
-		}
-		
-		Location exploreLoc = city;					// The following part is to extract the path from the links built step by step.
-		LinkedList<Location> shortestPath = new LinkedList<Location>();
-		
-		while (exploreLoc != null) {
-			shortestPath.addFirst(exploreLoc);
-			exploreLoc = exploreLoc.from;
-		}
-		
-		return shortestPath;
-	}
-	
-	public void printPathTo(Location city) {				// Displays the shortest path nicely. 
-		Iterator<Location> pathIterator = this.pathTo(city).iterator();
-		System.out.println("The path from " + this.name + " to " + city.name + " is:");
-		
-		while (pathIterator.hasNext()) {
-			pathIterator.next().distanceDisplay();
-		}
-		
-		this.resetPathFrom();
-	}
-	
-	private void resetPathFrom() {
-		this.distance = Double.POSITIVE_INFINITY;
-		this.from = null;
-		
-		for (int i = 0; i < this.neighbors.length; i++) {
-			if (this.neighbors[i].distance != Double.POSITIVE_INFINITY || this.neighbors[i].from != null) {
-				this.neighbors[i].resetPathFrom();
-			}
-		}
-	}
+/**
+ * Defines a vertex representing a city using a name and geodesic coordinates.
+ * 
+ * @author Paul Mabileau
+ */
+public class Location {
+    /**
+     * This structure can be used to implement graphs by calling setNeighbors.
+     */
+    private final String name;
+    private final double latitude, longitude;
+    private double distance;
+    /**
+     * The attribute <neighbors> is used to make the graph edges.
+     */
+    private Location[] neighbors;
+    /**
+     * <distance> and <from> are specifically meant for the path search.
+     */
+    private Location from;
+
+    public Location(final String name, final double latitude, final double longitude) {
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.distance = Double.POSITIVE_INFINITY;
+        this.from = null;
+    }
+
+    public Location(final String name, final double latitude, final double longitude,
+            final Location... neighbors) {
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.neighbors = neighbors;
+        this.distance = Double.POSITIVE_INFINITY;
+        this.from = null;
+    }
+
+    /**
+     * Standard display method.
+     */
+    public void display() {
+        System.out.println(this.name + ": (" + this.latitude + ", " + this.longitude + ")");
+    }
+
+    /**
+     * Display used only in the path search.
+     */
+    private void distanceDisplay() {
+        System.out.println("\t" + this.name + " at " + this.distance + " km");
+    }
+
+    /**
+     * Computes the length of the 3D arc going from Location <this> to <city> on
+     * the Earth' surface.
+     */
+    public double distanceTo(final Location city) {
+        final double lat1 = Math.PI * this.latitude / 180, lat2 = Math.PI * city.latitude / 180,
+                long1 = Math.PI * this.longitude / 180, long2 = Math.PI * city.longitude / 180;
+        return 6378 * (Math.PI / 2 - Math.asin(Math.sin(lat2) * Math.sin(lat1)
+                + Math.cos(long2 - long1) * Math.cos(lat2) * Math.cos(lat1)));
+    }
+
+    /**
+     * Sets <this> current vertex' neighboring Locations to the given
+     * <neighbors> Location array.
+     */
+    public void setNeighbors(final Location... neighbors) {
+        this.neighbors = neighbors;
+    }
+
+    /**
+     * Getter for path distance from starting point.
+     */
+    public double getDistance() {
+        return this.distance;
+    }
+
+    /**
+     * Uses Dijkstra's graph path search algorithm to compute the shortest path
+     * from <this> to <city>.
+     */
+    public LinkedList<Location> pathTo(final Location city) {
+        final LocationSet toVisit = new LocationSet(), newToVisit = new LocationSet();
+        // <toVisit> is a LocationSet containing the locations that have to be
+        // visited during the current step.
+        toVisit.add(this);
+        // <newToVisit> is used to receive the next locations needing to be
+        // visited and give them to <toVisit> after a step.
+        newToVisit.add(this);
+        // Distance from start to start: 0.
+        this.distance = 0;
+
+        // While there are still some locations to visit,
+        while (!toVisit.isEmpty()) {
+            // we visit them one by one,
+            for (int i = 0; i < toVisit.numLocations; i++) {
+                // and if the current location has neighbors,
+                if (toVisit.locations[i].neighbors != null) {
+                    // we go through them also;
+                    for (int j = 0; j < toVisit.locations[i].neighbors.length; j++) {
+                        // if the accumulated distance to the current neighbor
+                        // is smaller than previously,
+                        if (toVisit.locations[i] != city
+                                && toVisit.locations[i].distance + toVisit.locations[i].distanceTo(
+                                        toVisit.locations[i].neighbors[j]) < toVisit.locations[i].neighbors[j].distance) {
+                            // we update its parameters: the distance is
+                            // corrected, the path now goes through it, and it
+                            // needs to be visited during next step.
+                            toVisit.locations[i].neighbors[j].distance = toVisit.locations[i].distance
+                                    + toVisit.locations[i]
+                                            .distanceTo(toVisit.locations[i].neighbors[j]);
+                            toVisit.locations[i].neighbors[j].from = toVisit.locations[i];
+                            newToVisit.add(toVisit.locations[i].neighbors[j]);
+                        }
+                    }
+                }
+            }
+
+            // After each of every available location's neighbors has been
+            // processed, the next round of locations is loaded.
+            toVisit.become(newToVisit);
+            newToVisit.clear();
+        }
+
+        // The following part is to extract the path from the links built step
+        // by step.
+        Location exploreLoc = city;
+        final LinkedList<Location> shortestPath = new LinkedList<Location>();
+
+        while (exploreLoc != null) {
+            shortestPath.addFirst(exploreLoc);
+            exploreLoc = exploreLoc.from;
+        }
+
+        return shortestPath;
+    }
+
+    /**
+     * Displays the shortest path nicely.
+     */
+    public void printPathTo(final Location city) {
+        final Iterator<Location> pathIterator = this.pathTo(city).iterator();
+        System.out.println("The path from " + this.name + " to " + city.name + " is:");
+
+        while (pathIterator.hasNext()) {
+            pathIterator.next().distanceDisplay();
+        }
+
+        this.resetPathFrom();
+    }
+
+    private void resetPathFrom() {
+        this.distance = Double.POSITIVE_INFINITY;
+        this.from = null;
+
+        for (final Location neighbor: this.neighbors) {
+            if (neighbor.distance != Double.POSITIVE_INFINITY || neighbor.from != null) {
+                neighbor.resetPathFrom();
+            }
+        }
+    }
 }
--- Shortest Path/src/shortespath/LocationSet.java
+++ Shortest Path/src/shortespath/LocationSet.java
@@ -1,94 +1,127 @@
 package shortespath;
 
+/**
+ * Defines a set structure containing Location objects.
+ * 
+ * @author Paul Mabileau
+ */
+public class LocationSet {
+    /**
+     * Uses an extensible array as the source of memory.
+     */
+    Location[] locations;
+    /**
+     * To simulate the list's length.
+     */
+    int numLocations;
 
-public class LocationSet {												// Defines a set structure containing Location objects.
-	Location[] locations;										// Uses an extensible array as the source of memory.
-	int numLocations;											// To simulate the list's length.
-	
-	public LocationSet() {
-		this.locations = new Location[4];
-		this.numLocations = 0;
-	}
-	
-	public boolean isEmpty() {									// Returns true if and only if the set does not contain any element, i.e. non null objects.
-		for (int i = 0; i < this.numLocations; i++) {			// Used in path search.
-			if (this.locations[i] != null) {
-				return false;
-			}
-		}
-		return this.numLocations == 0;
-	}
-	
-	public void add(Location location) {						// Adds the given <location> at the end of the array,
-		if (this.numLocations == this.locations.length) {		// and manages the extensibility if required.
-			Location[] tmp = new Location[2 * this.numLocations];
-			
-			for (int i = 0; i < this.numLocations; i++) {
-				tmp[i] = this.locations[i];
-			}
-			
-			this.locations = tmp;
-		}
-		
-		this.locations[this.numLocations] = location;
-		this.numLocations += 1;
-	}
-	
-	public void add(Location... locations) {					// Adds multiple locations.
-		for (int i = 0; i < locations.length; i++) {
-			this.add(locations[i]);
-		}
-	}
-	
-	public void display() {										// Debug display.
-		System.out.println("locations.length: " + this.locations.length);
-		System.out.println("numLocations: " + this.numLocations);
-		System.out.println("locations:");
-		
-		for (int i = 0; i < this.numLocations; i++) {
-			this.locations[i].display();
-		}
-	}
-	
-	public Location removeMin() {								// Searches for the element having the minimum distance, removes it from the array and updates <numLocations>.
-		if (this.numLocations != 0) {
-			Location minLoc = this.locations[0];
-			double minDis = minLoc.getDistance();
-			int iMin = 0;
-			
-			for (int i = 1; i < this.numLocations; i++) {
-				if (this.locations[i].getDistance() < minDis) {
-					minLoc = this.locations[i];
-					minDis = minLoc.getDistance();
-					iMin = i;
-				}
-			}
-			
-			for (int i = iMin; i < this.numLocations - 1; i++) {
-				this.locations[i] = this.locations[i + 1];
-			}
-			
-			this.numLocations -= 1;
-			return minLoc;
-		}
-		else {
-			return null;
-		}
-	}
-	
-	public void clear() {										// Empties out completely the array, but preserves the same RAM space.
-		for (int i = 0; i < this.numLocations; i++) {
-			this.locations[i] = null;
-		}
-		this.numLocations = 0;
-	}
-	
-	public void become(LocationSet newSet) {					// Overwrites <this>'s elements by <newSet>'s. Enables LocationSet copying.
-		this.numLocations = newSet.numLocations;
-		this.locations = new Location[newSet.locations.length];
-		
-		for (int i = 0; i < newSet.numLocations; i++) {
-			this.locations[i] = newSet.locations[i];
-		}
-	}
+    public LocationSet() {
+        this.locations = new Location[4];
+        this.numLocations = 0;
+    }
+
+    /**
+     * Returns true if and only if the set does not contain any element, i.e.
+     * non null objects.
+     */
+    public boolean isEmpty() {
+        for (int i = 0; i < this.numLocations; i++) {
+            if (this.locations[i] != null) {
+                return false;
+            }
+        }
+        return this.numLocations == 0;
+    }
+
+    /**
+     * Adds the given <location> at the end of the array, and manages the
+     * extensibility if required.
+     */
+    public void add(final Location location) {
+        if (this.numLocations == this.locations.length) {
+            final Location[] tmp = new Location[2 * this.numLocations];
+
+            for (int i = 0; i < this.numLocations; i++) {
+                tmp[i] = this.locations[i];
+            }
+
+            this.locations = tmp;
+        }
+
+        this.locations[this.numLocations] = location;
+        this.numLocations += 1;
+    }
+
+    /**
+     * Adds multiple locations.
+     */
+    public void add(final Location... locations) {
+        for (final Location location: locations) {
+            this.add(location);
+        }
+    }
+
+    /**
+     * Debug display.
+     */
+    public void display() {
+        System.out.println("locations.length: " + this.locations.length);
+        System.out.println("numLocations: " + this.numLocations);
+        System.out.println("locations:");
+
+        for (int i = 0; i < this.numLocations; i++) {
+            this.locations[i].display();
+        }
+    }
+
+    /**
+     * Searches for the element having the minimum distance, removes it from the
+     * array and updates <numLocations>.
+     */
+    public Location removeMin() {
+        if (this.numLocations != 0) {
+            Location minLoc = this.locations[0];
+            double minDis = minLoc.getDistance();
+            int iMin = 0;
+
+            for (int i = 1; i < this.numLocations; i++) {
+                if (this.locations[i].getDistance() < minDis) {
+                    minLoc = this.locations[i];
+                    minDis = minLoc.getDistance();
+                    iMin = i;
+                }
+            }
+
+            for (int i = iMin; i < this.numLocations - 1; i++) {
+                this.locations[i] = this.locations[i + 1];
+            }
+
+            this.numLocations -= 1;
+            return minLoc;
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Empties out completely the array, but preserves the same RAM space.
+     */
+    public void clear() {
+        for (int i = 0; i < this.numLocations; i++) {
+            this.locations[i] = null;
+        }
+        this.numLocations = 0;
+    }
+
+    /**
+     * Overwrites <this>'s elements by <newSet>'s. Enables LocationSet copying.
+     */
+    public void become(final LocationSet newSet) {
+        this.numLocations = newSet.numLocations;
+        this.locations = new Location[newSet.locations.length];
+
+        for (int i = 0; i < newSet.numLocations; i++) {
+            this.locations[i] = newSet.locations[i];
+        }
+    }
 }
